6a598cefd1e7ccb0ccd07700b9e7955629db53d3
awsmrorg <awsmrorg@gmail.com>
2022-03-21 09:46:51 +0100

    version ~> 2.21.1

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "latest"
+      version = "~> 2.21.1"
     }
     aws = {
       source  = "hashicorp/aws"
